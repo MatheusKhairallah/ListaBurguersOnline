65974e8241d5c6b5731d45ec8f77d62f64bf4415
Matheus <playermaster10like@gmail.com>
2020-11-27 19:08:18 -0300

    tentativa de frontend

--- wwwroot/index.js
+++ wwwroot/index.js
@@ -1,5 +1,9 @@
 const iniciar=async () =>{
-    
+    const hamburguer = document.getElementById('hamburguer');
+
+    const response = await fetch('/Burguers');
+    const result= await response.json();
+    hamburguer.innerHTML= `${result.Nome} ${result.Preco}`;
 };
 
 document.addEventListener('DOMContentLoaded', iniciar);
